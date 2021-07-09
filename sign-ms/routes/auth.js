2e5b6d7d5e891614dca698d7fb8237c1630b7c32
SaraiRoiz-Git <53699451+SaraiRoiz-Git@users.noreply.github.com>
2021-07-09 18:22:03 +0300

    fix files + hash pasword +

--- sign-ms/routes/auth.js
+++ sign-ms/routes/auth.js
@@ -1,30 +1,55 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const userModel = require('../model/User')
-
-//VALIDATION
-const Joi = require('@hapi/joi')
-
-const schema = {
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-};
+const bcrypt = require('bcryptjs')
+const { regValidation, loginValidation } = require('../validation')
 
 router.post('/register', async (req, res) => {
-    //validate
-    const {error} = Joi.validate(req.body, schema)
+    // validate
+    // const {error} = regValidation(req.body)
+    // if(error){
+    //     return res.status(400).send(error.details[0].massage)
+    // }
+
+
+    //checking if mail exist
+    const emailExist = await User.findOne({ email: req.body.email })
+    if (emailExist) {
+        return res.status(400).send('Email is alredy exist')
+    }
+
+    //hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     try {
         const saveUser = await user.save()
-        res.send(saveUser);
-    } catch (err) {
+      //  res.send(saveUser);
+        res.send({user:user._id});
+    } catch (error) {
         res.status(400).send(err)
     }
 });
 
+//login
+
+router.post('/login',(req,res)=>{
+    // validate
+    // const {error} = loginValidation(req.body)
+    // if(error){
+    //     return res.status(400).send(error.details[0].massage)
+    // }
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+        return res.status(400).send('Email dosent exist')
+    }
+    //validate password
+    const 
+
+
+})
 module.exports = router;
