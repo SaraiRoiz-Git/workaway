2df73341e602842136787892cc967cc5dd1ef8cf
SaraiRoiz-Git <53699451+SaraiRoiz-Git@users.noreply.github.com>
2021-07-10 14:33:55 +0300

    fix server

--- sign-ms/index.js
+++ sign-ms/index.js
@@ -3,23 +3,30 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose');
 // import Routes
 const authRoute = require('./routes/auth');
-const cors =require('cors')
+const cors = require('cors')
 
 const app = express();
 dotenv.config();
 
 //db connect 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true,useUnifiedTopology:true },function(err){
-    if(err) console.log(err);
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+    if (err) console.log(err);
     console.log("database is connected");
 });
 
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 //middelware
- app.use(express.json())
+app.use(express.json())
 
 //Routh Midelware
 app.use('/api/user', authRoute);
-app.use(cors())
 
 const port = process.env.PORT || 300
 app.listen(port, () => {
