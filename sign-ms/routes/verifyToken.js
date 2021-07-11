de6baa936d8ca26f4820ccbd62343a66fbb50b01
SaraiRoiz-Git <53699451+SaraiRoiz-Git@users.noreply.github.com>
2021-07-11 19:26:35 +0300

    clean

--- sign-ms/routes/verifyToken.js
+++ sign-ms/routes/verifyToken.js
@@ -1,23 +1,7 @@
 const jwt = require('jsonwebtoken')
 
-// module.exports  = (req,res,next)=>{
-  
-//     const token = req.header.authtoken;
-//     if(!token){
-//         console.log(req.header)
-//         return res.status(401).send('access Denied');
-//     }
-//     try{
-//         const verified = jwt.verify(token,process.env.TOKEN_SECRET)
-//         req.user = verified;
-//         next
-//     }catch(err){
-//         return res.status(400).send('Invalid Token');
-//     }
-// }
-
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization;
     console.log(token)
     if (!token) {
         return res.status(401).send('access Denied');
